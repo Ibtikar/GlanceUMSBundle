4843ffd0b3c4800fce4943402ab46fa1449cb9ab
Ahmad Gamal <a.gamal@ibtikar.net.sa>
2017-01-10 15:01:41 +0200

    Backend | Add recipe articles add permission

--- Controller/RoleController.php
+++ Controller/RoleController.php
@@ -14,7 +14,7 @@ class RoleController extends BackendController {
     protected $translationDomain = 'role';
     protected  $tabs=array('user'=>array('title'=>'user','count'=>0,'modules'=>array('STAFF'=>array(),'ROLE'=>array(),'JOB'=>array(),'CITY'=>array()),'permission'=>array()),
         'recipeAndCategory'=>array('title'=>'recipeAndCategory','count'=>0,'modules'=>array('PRODUCT'=>array(),'SUBPRODUCT'=>array(),'CATEGORY'=>array(),'MAGAZINE'=>array()),'permission'=>array()),
-        'content'=>array('title'=>'content','count'=>0,'modules'=>array('RECIPE'=>array(),'RECIPENEW'=>array(),'RECIPEDELETED'=>array(),'RECIPEAUTOPUBLISH'=>array(),'RECIPEPUBLISH'=>array()),'permission'=>array()));
+        'content'=>array('title'=>'content','count'=>0,'modules'=>array('RECIPE'=>array(), 'BLOG'=>array(), 'RECIPENEW'=>array(),'RECIPEDELETED'=>array(),'RECIPEAUTOPUBLISH'=>array(),'RECIPEPUBLISH'=>array()),'permission'=>array()));
     protected $tabsnames=array('user','content');
     private $internalPermissions = array(
         'ROLE_ADMIN', 'ROLE_STAFF','ROLE_SUBPRODUCT_VIEW'
